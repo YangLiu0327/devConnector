9d2341e192623a18971690a22f5cebf1445e994a
liuyang <liuyangyangyang1992@gmail.com>
2022-02-06 14:58:15 +1100

    add store

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
-import React,  { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 
 const App = () => {
-  return (
+  <Provider store={store}>
     <Router>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path='register' element={<Register/>} />
-        <Route path='login' element={<Login/>} />
-      </Routes>
-    </Fragment>
-  </Router>
-  )
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
+        </Routes>
+      </Fragment>
+    </Router>
+  </Provider>
 }
-  
-
 
 export default App;
