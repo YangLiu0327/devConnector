f8e53b5470c948924dd16d982004686be3ff2a4a
liuyang <liuyangyangyang1992@gmail.com>
2022-02-10 14:52:55 +1100

    add posts func

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,6 +14,11 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         </Link>
       </li>
       <li>
+        <Link to='/posts'>
+          Posts
+        </Link>
+      </li>
+      <li>
         <i className='fas fa-user'></i>{' '}
         <Link to='/dashboard'>
           <span className='hide-sm'>Dashboard</span>
